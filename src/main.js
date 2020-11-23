fba63efa54d3fb14fdeddf0035c800287ead725e
KawhiLeonar <2539531364@qq.com>
2020-11-23 20:28:28 +0800

    项目初始化完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入element-ui组件库
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.less'
+import http from '@/utils/request.js'
+Vue.prototype.$http = http
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
